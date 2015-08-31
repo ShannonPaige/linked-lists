dec95efe65396e057956cb6e434cb3c0cb897240
ShannonPaige <spaige@gmail.com>
2015-08-31 10:26:48 -0600

    set up basic structure

---
+++ lib/list.rb
@@ -0,0 +1,59 @@
+module ShannonsLinkedList
+  class LinkedList
+
+    def initialize
+      # head of list
+      # data of each node
+      # next node of each node
+    end
+
+    def append
+      # append an element to the end of the list
+    end
+
+    def prepend
+      # prepend an element at the beginning of the list
+    end
+
+    def insert
+      #insert an element at an arbitrary position in the list
+    end
+
+    def includes?
+      # includes? gives back true or false whether the supplied value is in the list
+    end
+
+    def pop
+      # pop an element from the end of the list
+    end
+
+    def count
+      # count the number of elements in the list
+    end
+
+    def head
+      # return the head value at the beginning of the list
+    end
+
+    def tail
+      # return the tail value at the end of the list
+      # a tail is the node that has nil as its next node
+    end
+
+    def find_by_index
+      # find the value at a numeric position
+    end
+
+    def find_by_value
+      # finds the position of the first occurrence of a value
+    end
+
+    def remove_by_index
+      # removes the value at the specified index
+    end
+
+    def remove_by_value
+      # removes the first occurrence of the specified value
+    end
+  end
+end
---
+++ test/list_test.rb
@@ -0,0 +1,56 @@
+require "list"
+
+class ShannonsLinkedList::LinkedListTest < MiniTest::Test
+
+  def tests_empty_list
+    # expect nil to be the head
+  end
+
+  def tests_append
+    # append an element to the end of the list
+  end
+
+  def tests_prepend
+    # prepend an element at the beginning of the list
+  end
+
+  def tests_insert
+    #insert an element at an arbitrary position in the list
+  end
+
+  def tests_includes?
+    # includes? gives back true or false whether the supplied value is in the list
+  end
+
+  def tests_pop
+    # pop an element from the end of the list
+  end
+
+  def tests_count
+    # count the number of elements in the list
+  end
+
+  def tests_head
+    # return the head value at the beginning of the list
+  end
+
+  def tests_tail
+    # return the tail value at the end of the list
+  end
+
+  def tests_find_by_index
+    # find the value at a numeric position
+  end
+
+  def tests_find_by_value
+    # finds the position of the first occurrence of a value
+  end
+
+  def tests_remove_by_index
+    # removes the value at the specified index
+  end
+
+  def tests_remove_by_value
+    # removes the first occurrence of the specified value
+  end
+end
